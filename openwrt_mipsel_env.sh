b99fcf1c999c059211f97f33372db0f515a1e336
Steffen Dettmer <Steffen Dettmer steffen.dettmer@gmail.com>
2015-12-31 01:47:25 +0100

    Wake on Lan for openWRT: Readme.

---
+++ openwrt_mipsel_env.sh
@@ -0,0 +1,4 @@
+# source in shell to get mipsel cross compiler, Makefile
+# hardcodes gcc.
+export PATH="/home/steffen/work/openwrt/OpenWrt-SDK-Linux-i686-1/staging_dir_mipsel/mipsel-linux-uclibc/bin/:/home/steffen/download/OpenWrt-SDK-Linux-i686-1/staging_dir_mipsel/libexec/gcc/mipsel-linux-uclibc/3.4.4/:$PATH"
+
